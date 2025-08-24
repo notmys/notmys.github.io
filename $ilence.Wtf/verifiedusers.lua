f78b5dbd183e164e13fcbff7f38912b77eed9811
Mys (01xMYS) <braxton.frr@gmail.com>
2025-08-24 11:42:31 -0400

    Update verifiedusers.lua

--- $ilence.Wtf/verifiedusers.lua
+++ $ilence.Wtf/verifiedusers.lua
@@ -1,5 +1,12 @@
+-- verifiedusers.lua
 local verifiedPlayers = {
     [9247735263] = true,
     [87654321] = true,
-    [11223344] = true
 }
+return function(player, _)
+    if verifiedPlayers[player.UserId] then
+        return player.DisplayName .. utf8.char(0xE000)
+    else
+        return player.DisplayName
+    end
+end
