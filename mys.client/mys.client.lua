0ee778bb68a250d4bbcea901e427b6bb5806a6d0
Mys (01xMYS) <mysontop.co@gmail.com>
2025-09-13 00:00:51 -0400

    Update mys.client.lua

--- mys.client/mys.client.lua
+++ mys.client/mys.client.lua
@@ -1,6 +1,6 @@
 --[[
 mys.client v7.6.1 official Script FREE
---.gg/djMQRCNAAZ
-loadstring(game:HttpGet("https://www.utopia.voyage/mys.client/V7.6.1.lua"))()
+.gg/djMQRCNAAZ
 ]]
+
 loadstring(game:HttpGet("https://www.utopia.voyage/mys.client/V7.6.1.lua"))()
