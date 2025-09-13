28159f68b7aad98bb478818a248fc9f6dc7056e7
Mys (01xMYS) <mysontop.co@gmail.com>
2025-09-12 23:56:38 -0400

    Create mys.client.lua

---
+++ mys.client/mys.client.lua
@@ -0,0 +1,6 @@
+--[[
+mys.client v7.6.1 official Script FREE
+--.gg/djMQRCNAAZ
+loadstring(game:HttpGet("https://www.utopia.voyage/mys.client/V7.6.1.lua"))()
+]]
+loadstring(game:HttpGet("https://www.utopia.voyage/mys.client/V7.6.1.lua"))()
